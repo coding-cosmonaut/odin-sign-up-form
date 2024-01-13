40a3a5dbffbcf6a4f2c98d5c1c7ddba12df1d728
Alex <aamorgun2003@gmail.com>
2024-01-12 21:40:48 -0700

    Include functionality for inputs
    Added blur and focus events for the input that change the text of the
    span placeholder.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // <!--const Another red = d3003f-->
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((item) => {
+  item.addEventListener("focus", (event) => {
+    if (item.value) return;
+    item.nextElementSibling.textContent = compareNames(
+      item.nextElementSibling.textContent
+    );
+  });
+});
+
+inputs.forEach((item) => {
+  item.addEventListener("blur", (event) => {
+    console.log(item.value, "in blur");
+    if (!item.value) {
+      item.nextElementSibling.textContent = compareNames(
+        item.nextElementSibling.textContent
+      );
+    }
+  });
+});
+
+function compareNames(value) {
+  console.log(value, "compareNames");
+  switch (value) {
+    case "первый имя":
+      return "FIRST NAME";
+    case "FIRST NAME":
+      return "первый имя";
+    case "последний имя":
+      return "LAST NAME";
+    case "LAST NAME":
+      return "последний имя";
+    case "Эл. адрес":
+      return "EMAIL";
+    case "EMAIL":
+      return "Эл. адрес";
+    case "номер телефона":
+      return "PHONE NUMBER";
+    case "PHONE NUMBER":
+      return "номер телефона";
+    case "пароль":
+      return "PASSWORD";
+    case "PASSWORD":
+      return "пароль";
+    case "Подтвердите пароль":
+      return "Confirm Password";
+    case "Confirm Password":
+      return "Подтвердите пароль";
+    default:
+      return value;
+  }
+}
